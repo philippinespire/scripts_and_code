97ebd3643f145898994186f9491295b21978cbbf
ericgarciaresearch <40210956+ericgarciaresearch@users.noreply.github.com>
2020-10-30 11:50:23 -0400

    Update concat_fqFiles_diffLanes.sh

--- concat_fqFiles_diffLanes.sh
+++ concat_fqFiles_diffLanes.sh
@@ -1,7 +1,15 @@
-# example file name `20200331_PIRE-Ssp-CGub_052-Capture_CKDL200150313-1a-D712-AK1545_H7T2LBBXX_L7.R1.fq.gz`
+# Example file names of a single individual (052) with data from different lanes (L7 and L8):
+#`20200331_PIRE-Ssp-CGub_052-Capture_CKDL200150313-1a-D712-AK1545_H7T2LBBXX_L7.R1.fq.gz`
+#`20200331_PIRE-Ssp-CGub_052-Capture_CKDL200150313-1a-D712-AK1545_H7T2LBBXX_L8.R1.fq.gz`
+
+# User variables: Match the FILE_DIRECTION
 
 FILE_DIRECTION=R1
 THREADS=40
+
+# Also match the Lane info in the script below to your files (L7 and L8 in this example)
+
+#Script:
 
 # step 1: list all foward fq.gz files
 # ls *$FILE_DIRECTION.fq.gz | 
@@ -10,10 +18,11 @@ THREADS=40
 # sed 's/L[78].$FILE_DIRECTION.fq.gz//' | uniq | 
 
 # step 3: parallel cat files together
-# parallel --no-notice -j $THREADS "cat {}L[78].$FILE_DIRECTION.fq.gz > {}L78.$FILE_DIRECTION.fq.gz" 
+# parallel --no-notice -j $THREADS "cat {}L[78].$FILE_DIRECTION.fq.gz > {}L78.$FILE_DIRECTION.fq.gz"
 
 
-ls *$FILE_DIRECTION.fq.gz | sed 's/L[78].$FILE_DIRECTION.fq.gz//' | uniq | parallel --no-notice -j $THREADS "cat {}L[78].$FILE_DIRECTION.fq.gz > {}L78.$FILE_DIRECTION.fq.gz" 
+
+ls *$FILE_DIRECTION.fq.gz | sed 's/L[78].$FILE_DIRECTION.fq.gz//' | uniq | parallel --no-notice -j $THREADS "cat {}L[78].$FILE_DIRECTION.fq.gz > {}L78.$FILE_DIRECTION.fq.gz"
 
 ls *R1.fq.gz | sed 's/L[78].R1.fq.gz//' | less -S
 
